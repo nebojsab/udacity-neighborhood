33ef39a2ea2da5c2369bbd0c0cbf89618b96f6e8
Nebojsa Brnjasevic <nebojsa.brnjasevic@gmail.com>
2016-05-27 15:26:44 +0200

    Additional changes. Google maps api setup...

--- js/app.js
+++ js/app.js
@@ -71,19 +71,32 @@ function loadData() {
     return false;
 }
 
-function initialize() {
+var myCenter=new google.maps.LatLng(44.813505, 20.423412);
+var marker;
+
+function initialize()
+{
     var mapProp = {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
+        center:myCenter,
+        zoom:13,
         mapTypeId:google.maps.MapTypeId.ROADMAP
     };
+
     var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
-    var marker = new google.maps.Marker({
-        position: mapProp,
-        map: map,
-        title: 'Hello World!'
+
+    var marker=new google.maps.Marker({
+        position:myCenter,
     });
+
+    marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({
+        content:"Hello World!"
+    });
+
+    infowindow.open(map,marker);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
